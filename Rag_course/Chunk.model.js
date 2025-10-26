f9c05691d37b1e4bfb5ee1b41d1885e1741363f1
Fuad Sano <164843890+mroxygen2024@users.noreply.github.com>
2025-10-26 09:10:55 +0300

    Delete Rag_course directory

--- Rag_course/Chunk.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const chunkSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  metadata: { type: Object },
-  embedding: { type: [Number], required: true }
-});
-
-export default mongoose.model("Chunk", chunkSchema);
